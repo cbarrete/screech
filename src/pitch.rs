f59affc9a0a8dee230ec2047d988836068570483
Cédric Barreteau <cbarrete@users.noreply.github.com>
2021-01-22 21:28:12 +0100

    Make delay pitch general purpose

--- src/pitch.rs
+++ src/pitch.rs
@@ -6,44 +6,44 @@ pub trait Pitch {
 }
 
 impl Pitch for AudioBuffer {
-    // TODO use the factor to do something other than pitching down
     fn delay_pitch(mut self, factor: f32) -> AudioBuffer {
-        // TODO might want to parametrize the size
+        // TODO might want to parametrize the size and make it a power of 2 to use masks
+        if factor == 1.0 {
+            return self
+        }
+
         let buffer_size = std::cmp::min((self.metadata.sample_rate / 4) as usize, self.data.len());
         let mut buffer = vec![0.0; buffer_size];
-
         let channels = self.metadata.channels as usize;
         let samples_per_channel = self.data.len() / channels;
+
         for channel in 0..channels {
-            if factor < 1.0 {
+            if factor > 1.0 {
                 // initialize the buffer to avoid initial silence
                 for i in 0..buffer.len() {
                     buffer[i] = self.data[channel + i * channels];
                 }
             }
 
-            let mut divider = 0;
-            let mut read_index = 0;
-            let mut write_index = 0;
+            let mut read: f32 = 0.0;
+            let mut write = 0;
             for i in 0..samples_per_channel {
-                buffer[write_index] = self.data[channel + i * channels];
+                buffer[write] = self.data[channel + i * channels];
 
-                write_index += 1;
-                if write_index == buffer_size {
-                    write_index = 0;
+                write += 1;
+                if write == buffer_size {
+                    write = 0;
                 }
 
-                self.data[channel + i * channels] = buffer[read_index];
+                let first = read.floor();
+                let second = read.ceil();
+                let ratio = read - first;
+                self.data[channel + i * channels] = buffer[first as usize] * ratio + buffer[second as usize % buffer_size] * (1.0 - ratio);
 
-                divider += 1;
-                // TODO optimize with mask?
-                if divider == 2 {
-                    read_index += 1;
-                    divider = 0;
-                }
+                read += factor;
 
-                if read_index == buffer_size {
-                    read_index = 0;
+                if read >= buffer_size as f32 {
+                    read -= buffer_size as f32;
                 }
             }
         }

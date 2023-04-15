cd7d55fb28b2eef6fd1fd8d93e551118a16eeca2
Cédric Barreteau <cbarrete@users.noreply.github.com>
2023-04-15 15:43:37 -0400

    Add a nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  outputs = { self, nixpkgs }: {
+    packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      screech = pkgs.rustPlatform.buildRustPackage {
+        pname = "screech";
+        version = "0.1.0";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+
+      fzf_screech = pkgs.stdenvNoCC.mkDerivation {
+        pname = "fzf_screech.py";
+        version = "0.1.0";
+        src = ./.;
+        buildInputs = [ pkgs.python3 ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        installPhase = "mkdir -p $out/bin && cp fzf_screech.py $out/bin";
+        postFixup = "wrapProgram $out/bin/fzf_screech.py --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.fzf self.packages.${system}.screech ]}";
+      };
+
+      default = self.packages.${system}.screech;
+    });
+  };
+}

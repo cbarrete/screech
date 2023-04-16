968c9684a5efee34dea7c88a7b9bffedf6b18371
Cédric Barreteau <cbarrete@users.noreply.github.com>
2023-04-16 14:44:44 -0400

    fzf_screech: remove .py extension

--- flake.nix
+++ flake.nix
@@ -12,13 +12,13 @@
       };
 
       fzf_screech = pkgs.stdenvNoCC.mkDerivation {
-        pname = "fzf_screech.py";
+        pname = "fzf_screech";
         version = "0.1.0";
         src = ./.;
         buildInputs = [ pkgs.python3 ];
         nativeBuildInputs = [ pkgs.makeWrapper ];
-        installPhase = "mkdir -p $out/bin && cp fzf_screech.py $out/bin";
-        postFixup = "wrapProgram $out/bin/fzf_screech.py --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.fzf self.packages.${system}.screech ]}";
+        installPhase = "mkdir -p $out/bin && cp fzf_screech $out/bin";
+        postFixup = "wrapProgram $out/bin/fzf_screech --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.fzf self.packages.${system}.screech ]}";
       };
 
       default = self.packages.${system}.screech;
